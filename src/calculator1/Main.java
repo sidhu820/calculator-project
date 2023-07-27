a2d41648da8fdf7b8cb104a1127e2659ed89d07d
sidhu <sidhusooranadu151204@gmail.com>
2023-07-27 10:25:43 +0530

    intial commit completed work

---
+++ src/calculator1/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package calculator1;
+
+/**
+ *
+ * @author my pc
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Home obj=new Home();
+         new Home().setVisible(true);
+
+    }
+
+}
